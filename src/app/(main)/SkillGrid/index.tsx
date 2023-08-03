4e068855cf315f1ea22f251fccd76d39bc22e91b
temarych <teemest06@icloud.com>
2023-08-03 12:30:07 +0300

    Update UI

--- src/app/(main)/SkillGrid/index.tsx
+++ src/app/(main)/SkillGrid/index.tsx
@@ -2,7 +2,7 @@ import { Skill } from "./Skill";
 
 export const SkillGrid = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
       <Skill name="Photography" description="I have been taking photos for almost decade, and I have a lot of skills in this sphere" percentage={45} color="sky" />
       <Skill name="Photography" description="I have been taking photos for almost decade, and I have a lot of skills in this sphere" percentage={66} color="red" />
       <Skill name="Photography" description="I have been taking photos for almost decade, and I have a lot of skills in this sphere" percentage={34} color="purple" />
