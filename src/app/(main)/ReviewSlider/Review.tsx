76ec3e500780a5b0b8bae6c2088c06368df91eb1
temarych <teemest06@icloud.com>
2023-08-03 14:38:38 +0300

    Update review UI

--- src/app/(main)/ReviewSlider/Review.tsx
+++ src/app/(main)/ReviewSlider/Review.tsx
@@ -8,7 +8,7 @@ export interface ReviewProps {
 
 export const Review = ({ className }: ReviewProps) => {
   return (
-    <div className={twMerge(["relative border rounded-2xl p-8 min-h-[30em] flex flex-col gap-6", className])}>
+    <div className={twMerge(["relative border rounded-2xl p-6 min-h-[30em] flex flex-col gap-6", className])}>
       <Image
         alt       = "client"
         src       = "/client.png"
@@ -17,11 +17,11 @@ export const Review = ({ className }: ReviewProps) => {
         className = "rounded-full object-cover w-28 h-28"
       />
       <div className="flex flex-col gap-2">
-        <h1 className="font-bold text-2xl">Tymur Yavtushenko</h1>
+        <h1 className="font-bold text-xl">Tymur Yavtushenko</h1>
         <p className="text-gray-400">Client</p>
       </div>
-      <p className="h-full text-lg">Great photos and videos, thanks!</p>
-      <div className="flex flex-row gap-2 text-yellow-300 text-3xl">
+      <p className="h-full text-md">Great photos and videos, thanks!</p>
+      <div className="flex flex-row gap-2 text-yellow-300 text-2xl">
         <HiStar />
         <HiStar />
         <HiStar />
