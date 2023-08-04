5d0c9c7f6da7f1b11ab189ea3a992ab6e627b68a
temarych <teemest06@icloud.com>
2023-08-04 19:40:45 +0300

    Update header UI

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -3,15 +3,15 @@ import { IconButton }                                   from "../IconButton";
 
 export const NavBar = () => {
   return (
-    <div className="flex flex-row items-center gap-1">
+    <div className="flex flex-row items-center">
       <IconButton>
-        <HiOutlineHome className="text-lg" />
+        <HiOutlineHome className="text-xl" />
       </IconButton>
       <IconButton>
-        <HiOutlineHeart className="text-lg" />
+        <HiOutlineHeart className="text-xl" />
       </IconButton>
       <IconButton>
-        <HiOutlineUser className="text-lg" />
+        <HiOutlineUser className="text-xl" />
       </IconButton>
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,8 @@ import { Tag }      from "../Tag";
 export const Header = () => {
   return (
     <Fragment>
-      <div className="relative w-full h-14 border-b border-transparent" />
-      <header className="fixed z-10 w-full h-14 px-4 flex flex-row justify-center border-b border-gray-100 bg-white">
+      <div className="relative w-full h-16 border-b border-transparent" />
+      <header className="fixed z-10 w-full h-16 px-4 flex flex-row justify-center border-b border-gray-100 bg-white">
         <div className="w-full max-w-[80em] flex flex-row items-center">
           <div className="flex-1 flex items-center justify-start">
             <Tag>Portfolio</Tag>
