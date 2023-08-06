673f3056ef8f6110d056ee4d326b368ab6e0089c
temarych <teemest06@icloud.com>
2023-08-06 15:00:24 +0300

    Update navigation

--- src/app/(main)/Header/NavBar.tsx
+++ src/app/(main)/Header/NavBar.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { HiOutlineCamera, HiOutlineHome } from "react-icons/hi";
+import { useRouter }                      from "next/navigation";
 import { IconButton }                     from "@components/IconButton";
 
 export const NavBar = () => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-row items-center">
-      <IconButton>
+      <IconButton onClick={() => router.push("/")}>
         <HiOutlineHome className="text-xl" />
       </IconButton>
-      <IconButton>
+      <IconButton onClick={() => router.push("/gallery")}>
         <HiOutlineCamera className="text-xl" />
       </IconButton>
     </div>
---
+++ src/app/(main)/gallery/page.tsx
@@ -0,0 +1,7 @@
+const Gallery = () => {
+  return (
+    <h1 className="text-center">Gallery</h1>
+  );
+};
+
+export default Gallery;
