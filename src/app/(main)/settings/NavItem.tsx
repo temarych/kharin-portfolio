96f067168b860a4e542f0f96dd2786160d31ebcc
temarych <teemest06@icloud.com>
2023-08-08 15:31:57 +0300

    Update navbar

--- src/app/(main)/settings/NavItem.tsx
+++ src/app/(main)/settings/NavItem.tsx
@@ -1,16 +1,29 @@
-import { ReactNode } from "react";
-import { List }      from "@components/List";
+"use client";
+
+import { ReactNode }              from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { twMerge }                from "tailwind-merge";
+import { List }                   from "@components/List";
 
 export interface NavItemProps {
-  icon: ReactNode;
-  name: string;
+  icon      : ReactNode;
+  activeIcon: ReactNode;
+  name      : string;
+  path      : string;
 }
 
-export const NavItem = ({ icon, name }: NavItemProps) => {
+export const NavItem = ({ icon, activeIcon, name, path }: NavItemProps) => {
+  const router   = useRouter();
+  const pathname = usePathname();
+  const isActive = pathname === path;
+
   return (
     <List.ItemWrapper>
-      <List.ItemButton>
-        <List.ItemContent leftAdornment={icon}>
+      <List.ItemButton 
+        onClick   = {() => router.push(path)} 
+        className = {twMerge([isActive ? "font-bold" : null])}
+      >
+        <List.ItemContent leftAdornment={isActive ? activeIcon : icon}>
           {name}
         </List.ItemContent>
       </List.ItemButton>
