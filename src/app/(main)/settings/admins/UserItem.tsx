2fa5f51b75fe9dafb415ebc9855b3f43d7061470
temarych <teemest06@icloud.com>
2023-08-08 20:09:20 +0300

    Update admins page UI

--- src/app/(main)/settings/admins/UserItem.tsx
+++ src/app/(main)/settings/admins/UserItem.tsx
@@ -16,13 +16,13 @@ export const UserItem = ({ firstName, lastName, email }: UserItemProps) => {
     <List.ItemWrapper>
       <List.ItemTabDiv>
         <List.ItemContent 
+          className      = "py-3"
           leftAdornment  = {<Avatar className="border" />} 
           rightAdornment = {(
             <IconButton>
-              <HiX className="text-lg text-gray-400" />
+              <HiX className="text-xl text-gray-400" />
             </IconButton>
           )}
-          className      = "py-3"
         >
           <h1 className="font-bold text-md">{firstName} {lastName}</h1>
           <p className="text-sm text-gray-400">{email}</p>
