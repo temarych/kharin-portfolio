29ed6f87c49976ab361846b9ec37464b0b1ae826
temarych <teemest06@icloud.com>
2023-08-12 18:19:09 +0300

    Update UI

--- src/app/(main)/gallery/add/photo/PhotoDetail.tsx
+++ src/app/(main)/gallery/add/photo/PhotoDetail.tsx
@@ -9,8 +9,8 @@ export const PhotoDetail = ({ name, value }: PhotoDetailProps) => {
   return (
     <div className="flex flex-row items-center gap-4">
       <div className="flex flex-col">
-        <p className="text-md text-gray-400">{name}</p>
-        <h1 className="text-xl">{value}</h1>
+        <p className="text-sm text-gray-400">{name}</p>
+        <h1 className="text-lg">{value}</h1>
       </div>
     </div>
   );
