cdcd161a9d17d9562954e063fcc8ce8ae1f11ca7
temarych <teemest06@icloud.com>
2023-08-12 20:49:22 +0300

    Refresh photos on adding

--- src/app/(main)/gallery/add/photo/page.tsx
+++ src/app/(main)/gallery/add/photo/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
+import { useRouter }                        from "next/navigation";
 import { revalidate }                       from "@utils/revalidate";
+import { usePhotos }                        from "@hooks/usePhotos";
 import { Photo, PickPhoto }                 from "./PickPhoto";
 import { ViewPhoto }                        from "./ViewPhoto";
 
 const AddPhoto = () => {
+  const router                        = useRouter();
   const [isUploading, setIsUploading] = useState(false);
   const [photo, setPhotoPlain]        = useState<Photo | null>(null);
+  const { refreshPhotos }             = usePhotos();
 
   useEffect(
     () => () => {
@@ -44,11 +48,11 @@ const AddPhoto = () => {
         method: "POST",
         body  : formData
       });
+      await refreshPhotos();
       await revalidate("photos");
-      setPhoto(null);
-      setIsUploading(false);
+      router.push("/gallery");
     },
-    [photo, isUploading, setPhoto]
+    [photo, isUploading, refreshPhotos, router]
   );
 
   return !photo ? (
